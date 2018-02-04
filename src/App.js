a26ef5e60ff3a98e35c1d4bc63d0307dc7fcbd52
Todd Matthews <gtmatthews@gmail.com>
2018-02-04 14:49:13 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 className="App-title">Tapify</h1>
+        <p className="App-intro">Tap the spacebar to give the tempo and return to lock it in</p>
+      </div>
+    );
+  }
+}
+
+export default App;
