616d906bfd30db62123899fe03c1d9f03cd721f8
Todd Matthews <gtmatthews@gmail.com>
2018-02-04 17:35:24 -0500

    Add average tempo state and spotify login button

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    averageTempo: 0,
+  };
   componentDidMount() {
     let tempo = [];
     let taps = [];
     document.body.addEventListener('keydown', e => {
+      //reset tempo
+      if (e.keyCode === 82) {
+        console.log('r');
+        tempo = [];
+        taps = [];
+        this.setState({ averageTempo: 0 });
+      }
       //calculate tempo
       if (e.keyCode === 32 && e.keyCode !== 82) {
         tempo.push(new Date().getTime());
@@ -16,8 +26,8 @@ class App extends Component {
       if (taps.length >= 2 && e.keyCode !== 82) {
         let average = taps.reduce((a, b) => a + b, 0) / taps.length;
         let bpm = (60000 / average).toFixed(2);
-
         console.log(`Your average tempo is ${bpm} BPM`);
+        this.setState({ averageTempo: bpm });
       }
     });
   }
@@ -26,8 +36,11 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="App-title">Tapify</h1>
-        <p className="App-intro">Tap the spacebar to give the tempo and return to lock it in</p>
-        <h2>Your tempo is {}</h2>
+        <p className="App-intro">Tap the 'spacebar' to calculate tempo and 'r' to reset</p>
+        <h2>Your tempo is {Math.floor(this.state.averageTempo)} BPM</h2>
+        <button class="btn" role="button" href="#">
+          Login to Spotify
+        </button>
       </div>
     );
   }
